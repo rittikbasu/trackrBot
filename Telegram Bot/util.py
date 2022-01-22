10df4e5eaa6c578bee21fd96eed4b827485c0f90
Rittik Basu <78151199+rittikbasu@users.noreply.github.com>
2022-01-22 17:06:32 +0530

    v2.0

--- Telegram Bot/util.py
+++ Telegram Bot/util.py
@@ -1,3 +1,23 @@
+from replit import db
+
+def watchlisted(key,username):
+    text = f"<b>{username}'s Watchlist</b>\n\n"
+
+    if key in db.keys():
+        count = 0
+        for product in db[key]:
+            count += 1
+            alert = product.get('alert', 'None')
+            title = product.get('title', 'None')
+            url = product.get('url', 'None')
+
+            text += f'{count})\n<b>Name:</b> <a href="{url}">{title}</a>\n<b>Alert:</b> {alert}\n\n'
+    else:
+        text += '<code>Empty</code>'
+
+    return text
+
+
 def is_number(s):
     try:
         float(s) # for int, long and float
